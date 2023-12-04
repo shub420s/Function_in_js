20edc9e66f3e9f8d3bbd3df72acbeb0949918123
shub420s <144937257+shub420s@users.noreply.github.com>
2023-12-04 15:48:42 +0000

    function_in_js.js

---
+++ function_in_js.js
@@ -0,0 +1,14 @@
+//Ans 1
+function arrowsquare(num) {
+
+   let squarevalue = num*num;
+
+    return squarevalue;
+
+}
+     let result = arrowsquare(5);
+      console.log(result);
+      
+
+      //ANS2
+      
